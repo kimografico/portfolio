796f019d203cb689e1e9c0becd24ecf2ab93f20e
Carlos Sáez <carlos.saez.blanco.contractor@bbva.com>
2026-05-16 10:36:47 +0200

    add some icons

---
+++ src/components/iconos/IconRefresh.tsx
@@ -0,0 +1,27 @@
+import type { IconProps } from '../../types/icons';
+
+export function IconRefresh({
+  size = 24,
+  color = 'currentColor',
+  strokeWidth = 2,
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
+      <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
+    </svg>
+  );
+}
---
+++ src/components/iconos/IconServer.tsx
@@ -0,0 +1,30 @@
+import type { IconProps } from '../../types/icons';
+
+export function IconServer({
+  size = 24,
+  color = 'currentColor',
+  strokeWidth = 2,
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M3 7a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3" />
+      <path d="M15 20h-9a3 3 0 0 1 -3 -3v-2a3 3 0 0 1 3 -3h12" />
+      <path d="M7 8v.01" />
+      <path d="M7 16v.01" />
+      <path d="M20 15l-2 3h3l-2 3" />
+    </svg>
+  );
+}
